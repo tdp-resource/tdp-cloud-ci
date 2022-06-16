7f6fd9ee90e8a71846afdbf45d9acd40c093c734
若海 <wang@rehiy.com>
2022-06-17 03:17:43 +0800

    update

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@
 git clone https://github.com/tdp-resource/tdp-cloud.git
 git clone https://github.com/tdp-resource/tdp-cloud-ui.git
 
+set
+
 chmod +x */build.sh
 
 cd ./tdp-cloud-ui
 npm i && ./build.sh
 
-sleep 1
-cp -av build/* ../tpd-cloud/front/
+cd ../
+cp -av ./tdp-cloud-ui/build/* ./tpd-cloud/front/
 
-cd ../tdp-cloud
+cd ./tdp-cloud
 go mod tidy && ./build.sh
